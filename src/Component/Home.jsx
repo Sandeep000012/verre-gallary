577fea1778bbfe7c251be0f3959585945aa753e7
Sandeep000012 <sandeepmangloda@gmail.com>
2025-05-26 21:27:38 +0530

    some updation

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 3000,
     pauseOnHover: false,
     arrows: false,
